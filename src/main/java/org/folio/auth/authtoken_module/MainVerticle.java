9396b39981de4284cf8756a14456baf5712dabe4
David Crossley <dcrossley@indexgeo.com.au>
2017-02-20 14:38:22 +1100

    Remove old default IDE license headers

--- src/main/java/org/folio/auth/authtoken_module/MainVerticle.java
+++ src/main/java/org/folio/auth/authtoken_module/MainVerticle.java
@@ -36,12 +36,6 @@ import java.util.regex.Pattern;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author kurt
