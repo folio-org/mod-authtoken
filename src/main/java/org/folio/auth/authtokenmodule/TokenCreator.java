ccb0a8b53c7c05f0d07e6412afca05b294dc230e
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2018-09-06 21:13:03 +0000

    Implement endpoints for refresh tokens and encoded tokens. Begin tests

--- src/main/java/org/folio/auth/authtokenmodule/TokenCreator.java
+++ src/main/java/org/folio/auth/authtokenmodule/TokenCreator.java
@@ -1,43 +1,124 @@
 package org.folio.auth.authtokenmodule;
 
 
+/*
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.crypto.MacProvider;
+*/
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
+/*
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
+*/
+import com.nimbusds.jose.EncryptionMethod;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWEAlgorithm;
+import com.nimbusds.jose.JWEHeader;
+import com.nimbusds.jose.JWEObject;
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jose.Payload;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.KeyLengthException;
+import com.nimbusds.jose.crypto.DirectDecrypter;
+import com.nimbusds.jose.crypto.DirectEncrypter;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.text.ParseException;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import org.folio.auth.authtokenmodule.BadSignatureException;
+
 
 
 public class TokenCreator {
-  private Key JWTSigningKey;
-  private JwtParser parser;
-  private static final SignatureAlgorithm JWTAlgorithm = SignatureAlgorithm.HS512;
+  private SecretKey JWTSigningKey;
+  //private JwtParser parser;
+  //private static final SignatureAlgorithm JWTAlgorithm = SignatureAlgorithm.HS512;
+  private static final EncryptionMethod encryptionMethod = EncryptionMethod.A256CBC_HS512;
+  private int keyBitLength;
+  private SecretKeyFactory keyFactory;
+  private byte[] sharedKey;
+  private MACSigner macSigner;
+  private MACVerifier macVerifier;
+  private DirectEncrypter encrypter;
+  private DirectDecrypter decrypter;
+  
 
-  public TokenCreator(String key) {
-    if(key != null) {
-      JWTSigningKey = new SecretKeySpec(key.getBytes(), JWTAlgorithm.getJcaName());
-    } else {
-      JWTSigningKey = MacProvider.generateKey(JWTAlgorithm);
-    }
-    parser = Jwts.parser().setSigningKey(JWTSigningKey);
+  public TokenCreator(String key) throws KeyLengthException, JOSEException {
+   if(key != null) {
+     //pad to minimum length
+     if(key.length() < 32) {
+       key = String.format("%-32s", key);
+     }
+     //sharedKey = key.getBytes();
+     init(key.getBytes());
+   } else {
+     //sharedKey = new byte[64];
+     byte[] tempKey = new byte[64];
+     new SecureRandom().nextBytes(tempKey);
+     init(tempKey);
+   }
+  }
+  
+  public TokenCreator(byte[] byteArray) throws KeyLengthException, JOSEException {
+    //sharedKey = byteArray;
+    init(byteArray);
+  }
+  
+  private void init(byte[] setKey) throws KeyLengthException, JOSEException {
+    sharedKey = setKey;
+    macSigner = new MACSigner(sharedKey);
+    macVerifier = new MACVerifier(sharedKey);
+    encrypter = new DirectEncrypter(sharedKey);
+    decrypter = new DirectDecrypter(sharedKey);
   }
 
   /*
    * payload should be an encoded JSON object
+   * I know 'JWTToken' is redundant, but I don't care...muahahahaha
    */
-  public String createToken(String payload) {
-    String token = Jwts.builder()
-      .signWith(JWTAlgorithm, JWTSigningKey)
-      .setPayload(payload)
-      .compact();
-    return token;
+  public String createJWTToken(String payload) throws JOSEException, ParseException {
+    JWTClaimsSet claims = JWTClaimsSet.parse(payload);
+    SignedJWT jwt = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claims);
+    jwt.sign(macSigner);
+    return jwt.serialize();
   }
 
-  public void checkToken(String token) throws SignatureException, MalformedJwtException, UnsupportedJwtException {
-    parser.parseClaimsJws(token);
+  public void checkJWTToken(String token) throws JOSEException, ParseException,
+      BadSignatureException {
+    //parser.parseClaimsJws(token);
+    SignedJWT jwt = SignedJWT.parse(token);
+    if(!jwt.verify(macVerifier)) {
+      String message = String.format("Could not verify token %s", token);
+      throw(new BadSignatureException(message));
+    };
+  }
+  
+  public String createJWEToken(String payloadString) throws JOSEException {
+    Payload payload = new Payload(payloadString);
+    JWEHeader header = new JWEHeader(JWEAlgorithm.DIR, EncryptionMethod.A256GCM);
+    JWEObject jwe = new JWEObject(header, payload);
+    jwe.encrypt(encrypter);
+    return jwe.serialize();
+  }
+  
+  public String decodeJWEToken(String token) throws JOSEException, ParseException {
+    JWEObject jwe = JWEObject.parse(token);
+    jwe.decrypt(decrypter);
+    return jwe.getPayload().toString();
   }
 }
