f00085835e598401eae4519d8dc1ac6677e25648
Adam Dickmeiss <adam@indexdata.dk>
2019-09-22 21:16:14 +0200

    Reformat AuthRoutingEntry

--- src/main/java/org/folio/auth/authtokenmodule/AuthRoutingEntry.java
+++ src/main/java/org/folio/auth/authtokenmodule/AuthRoutingEntry.java
@@ -17,23 +17,25 @@ import static org.folio.auth.authtokenmodule.MainVerticle.getClaims;
 
 /**
  *
- * @author kurt
- * This class defines the values needed to create a non-filter endpoint in the authtoken module
+ * @author kurt This class defines the values needed to create a non-filter
+ * endpoint in the authtoken module
  */
 public class AuthRoutingEntry {
+
   private String endpoint;
   private List<String> requiredPermissions;
   private Handler<RoutingContext> handler;
   private final Logger logger = LoggerFactory.getLogger("mod-auth-authtoken-module");
 
   public AuthRoutingEntry(String endpoint, String[] requiredPermissions,
-      Handler<RoutingContext> handler) {
+    Handler<RoutingContext> handler) {
     init(endpoint, new ArrayList<>(Arrays.asList(requiredPermissions)), handler);
   }
+
   private void init(String endpoint, List<String> requiredPermissions,
-      Handler<RoutingContext> handler) {
+    Handler<RoutingContext> handler) {
     this.endpoint = endpoint;
-    if(requiredPermissions == null) {
+    if (requiredPermissions == null) {
       this.requiredPermissions = new ArrayList<>();
     } else {
       this.requiredPermissions = requiredPermissions;
@@ -55,42 +57,42 @@ public class AuthRoutingEntry {
 
   /*
   Return true if we're handling the route, false if pass-thru
-  */
+   */
   public boolean handleRoute(RoutingContext ctx, String authToken, String moduleTokens) {
     JsonObject claims = getClaims(authToken);
     JsonArray extraPermissions = claims.getJsonArray("extra_permissions");
-    if(extraPermissions == null) {
+    if (extraPermissions == null) {
       extraPermissions = new JsonArray();
     }
-    if(!ctx.request().path().startsWith(endpoint)) {
+    if (!ctx.request().path().startsWith(endpoint)) {
       return false;
     }
-    if(ctx.getBodyAsString() == null || ctx.getBodyAsString().isEmpty()) {
+    if (ctx.getBodyAsString() == null || ctx.getBodyAsString().isEmpty()) {
       logger.debug(String.format("No body found in request for %s, treating as filter", endpoint));
       //check for permissions
       boolean allFound = true;
-      for(String perm : requiredPermissions) {
-        if(!extraPermissions.contains(perm)) {
+      for (String perm : requiredPermissions) {
+        if (!extraPermissions.contains(perm)) {
           allFound = false;
           break;
         }
       }
-      if(!allFound) {
+      if (!allFound) {
         logger.error(String.format("Insufficient permissions to access endpoint %s", endpoint));
         ctx.response()
-            .setChunked(true)
-            .setStatusCode(401)
-            .end(String.format("Missing required module-level permissions for endpoint '%s'",
-                endpoint));
+          .setChunked(true)
+          .setStatusCode(401)
+          .end(String.format("Missing required module-level permissions for endpoint '%s'",
+            endpoint));
       } else {
         String magicPermission = getMagicPermission(endpoint);
         ctx.response()
-            .setChunked(true)
-            .setStatusCode(202)
-            .putHeader(PERMISSIONS_HEADER, new JsonArray().add(magicPermission).encode())
-            .putHeader(OKAPI_TOKEN_HEADER, authToken)
-            .putHeader(MODULE_TOKENS_HEADER, moduleTokens)
-            .end();
+          .setChunked(true)
+          .setStatusCode(202)
+          .putHeader(PERMISSIONS_HEADER, new JsonArray().add(magicPermission).encode())
+          .putHeader(OKAPI_TOKEN_HEADER, authToken)
+          .putHeader(MODULE_TOKENS_HEADER, moduleTokens)
+          .end();
       }
       return true;
     } else {
@@ -98,24 +100,24 @@ public class AuthRoutingEntry {
       JsonArray requestPerms = null;
       try {
         requestPerms = new JsonArray(ctx.request().headers().get(PERMISSIONS_HEADER));
-      } catch(io.vertx.core.json.DecodeException dex) {
+      } catch (io.vertx.core.json.DecodeException dex) {
         logger.warn(String.format("Error parsing permissions header: %s",
-            dex.getLocalizedMessage()));
+          dex.getLocalizedMessage()));
         logger.warn(String.format("Headers are: %s", ctx.request().headers().toString()));
       }
       boolean passThrough = false;
-      if(requestPerms != null && requestPerms.contains(getMagicPermission(endpoint)) ) {
+      if (requestPerms != null && requestPerms.contains(getMagicPermission(endpoint))) {
         passThrough = true;
       }
-      if(passThrough) {
+      if (passThrough) {
         logger.info(String.format("Calling handler %s", handler.toString()));
         handler.handle(ctx);
       } else {
         logger.error(String.format("Missing assigned permission to access endpoint '%s'",
-            endpoint));
+          endpoint));
         ctx.response()
-            .setStatusCode(403)
-            .end(String.format("Missing permissions to access endpoint '%s'", endpoint));
+          .setStatusCode(403)
+          .end(String.format("Missing permissions to access endpoint '%s'", endpoint));
       }
       return true;
     }
