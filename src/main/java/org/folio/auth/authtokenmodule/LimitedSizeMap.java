57eba8cc73b5b66e104a087f1dbc9514dc13e3d0
Adam Dickmeiss <adam@indexdata.dk>
2019-09-23 15:01:06 +0200

    Implement hashCode too

--- src/main/java/org/folio/auth/authtokenmodule/LimitedSizeMap.java
+++ src/main/java/org/folio/auth/authtokenmodule/LimitedSizeMap.java
@@ -3,13 +3,14 @@ package org.folio.auth.authtokenmodule;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-
 /**
  *
  * @author kurt
  */
-public class LimitedSizeMap<K,V> extends LinkedHashMap<K, V> {
+public class LimitedSizeMap<K, V> extends LinkedHashMap<K, V> {
+
   private final int maxSize;
+
   public LimitedSizeMap(int maxSize) {
     this.maxSize = maxSize;
   }
@@ -26,5 +27,10 @@ public class LimitedSizeMap<K,V> extends LinkedHashMap<K, V> {
     }
     LimitedSizeMap fobj = (LimitedSizeMap) obj;
     return fobj.maxSize == maxSize;
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode() + maxSize;
   }
 }
--- src/test/java/org/folio/auth/authtokenmodule/LimitedSizeMapTest.java
+++ src/test/java/org/folio/auth/authtokenmodule/LimitedSizeMapTest.java
@@ -2,23 +2,25 @@ package org.folio.auth.authtokenmodule;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 public class LimitedSizeMapTest {
-  
+
   @Test
   public void test1() {
-    LimitedSizeMap<String,String> map2 = new LimitedSizeMap<>(2);
-    LimitedSizeMap<String,String> map2a = new LimitedSizeMap<>(2);
-    LimitedSizeMap<String,String> map1 = new LimitedSizeMap<>(1);
-    
+    LimitedSizeMap<String, String> map2 = new LimitedSizeMap<>(2);
+    LimitedSizeMap<String, String> map2a = new LimitedSizeMap<>(2);
+    LimitedSizeMap<String, String> map1 = new LimitedSizeMap<>(1);
+
     Assert.assertTrue(map2.equals(map2));
     Assert.assertTrue(map2.equals(map2a));
+    Assert.assertEquals(map2.hashCode(), map2a.hashCode());
     Assert.assertFalse(map2.equals(map1));
-    
+
     map2.put("k1", "v1");
     Assert.assertNotNull(map2.get("k1"));
     map2.put("k2", "v2");
     Assert.assertNotNull(map2.get("k1"));
     map2.put("k3", "v3");
     Assert.assertNull(map2.get("k1"));
-  }  
+  }
 }
