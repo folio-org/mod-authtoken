2284df955a8911c38b3e1d50bfe40f4698765e2f
Adam Dickmeiss <adam@indexdata.dk>
2020-12-17 11:35:31 +0100

    LimitedQueueQueue separate; unit test

---
+++ src/main/java/org/folio/auth/authtokenmodule/LimitedSizeQueue.java
@@ -0,0 +1,21 @@
+package org.folio.auth.authtokenmodule;
+
+import java.util.ArrayList;
+
+public class LimitedSizeQueue<K> extends ArrayList<K> {
+
+  private final int maxSize;
+
+  public LimitedSizeQueue(int size){
+    this.maxSize = size;
+  }
+
+  @Override
+  public boolean add(K k){
+    boolean r = super.add(k);
+    if (size() > maxSize) {
+      removeRange(0, size() - maxSize - 1);
+    }
+    return r;
+  }
+}
---
+++ src/test/java/org/folio/auth/authtokenmodule/LimitedSizeQueueTest.java
@@ -0,0 +1,24 @@
+package org.folio.auth.authtokenmodule;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LimitedSizeQueueTest {
+  @Test
+  public void testQueueFull() {
+    LimitedSizeQueue<Integer> q = new LimitedSizeQueue<>(1);
+    Assert.assertTrue(q.isEmpty());
+    q.add(1);
+    Assert.assertEquals(1, q.size());
+    Assert.assertTrue(q.contains(1));
+    q.add(2);
+    Assert.assertEquals(2, q.size()); // size is bigger than max size (1)
+    Assert.assertTrue(q.contains(1));
+    Assert.assertTrue(q.contains(2));
+    q.add(3);
+    Assert.assertEquals(2, q.size()); // at least here it's limited
+    Assert.assertFalse(q.contains(1));
+    Assert.assertTrue(q.contains(2));
+    Assert.assertTrue(q.contains(3));
+  }
+}
